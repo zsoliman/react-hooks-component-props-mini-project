633a27da920f8be4fa63bcc2f75d3bd758d7a201
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-21 14:55:03 -0400

    site works

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Header from "../components/Header";
+import Header from "../components/Header.js";
 
 test("renders a <header> element", () => {
   const { container } = render(<Header />);
